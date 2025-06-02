60e93bc5c7b880f669de7cc06a276ff343b482c2
maxwell5782 <maxwell5782@hotmail.com>
2025-06-02 15:43:06 +0800

    Update getScreenSize.lua

--- getScreenSize.lua
+++ getScreenSize.lua
@@ -1,4 +1,8 @@
+setImmersiveMode(true)
+autoGameArea(true)
+
 local screen = getAppUsableScreenSize()
 print(string.format("Usable %s*%s", screen:getX(), screen:getY()))
 screen = getRealScreenSize()
 print(string.format("Screen %s*%s", screen:getX(), screen:getY()))
+print(getGameArea())
