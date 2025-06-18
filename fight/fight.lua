cf11cfc58a818ca8b3a99d7e9e954fd623d94a6f
許介人 <maxwell5782@hotmail.com>
2025-06-18 23:03:31 +0800

    持續點擊開戰圖示的腳本

---
+++ fight/fight.lua
@@ -0,0 +1,20 @@
+-- ========== 設定 ================
+Settings:setCompareDimension(true, 2340)
+Settings:setScriptDimension(true, 2340)
+Settings:set("MinSimilarity", 0.9)
+setImmersiveMode(true)
+autoGameArea(true)
+setManualTouchParameter(20, 1)
+
+actionList = { {action = "touchDown", target = Location(800,350)},
+    {action = "touchMove", target = Location(1100,350)},
+    {action = "touchUp", target = Location(1100,350)} }
+
+while true do 
+ if exists("fight.png", 0.5) 
+ then
+    click(getLastMatch())
+ else
+  manualTouch(actionList)
+ end
+end
