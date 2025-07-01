90f2da906b40a0f39e5e0c795255313ae4bc8b29
maxwell.hsu <maxwell.hsu@chroma.com.tw>
2025-07-01 15:29:29 +0800

    調整相似度要求

--- workshop/workshop.lua
+++ workshop/workshop.lua
@@ -1,7 +1,7 @@
 -- ========== 設定 ================
 Settings:setCompareDimension(true, 2340)
 Settings:setScriptDimension(true, 2340)
-Settings:set("MinSimilarity", 0.8)
+Settings:set("MinSimilarity", 0.9)
 setImmersiveMode(true)
 autoGameArea(true)
 setManualTouchParameter(20, 1)
