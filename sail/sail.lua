2ee83f87e17d0e98bec1eb7dbb37d4e67e6936c1
許介人 <maxwell5782@hotmail.com>
2025-07-05 00:00:57 +0800

    純航行點操帆跟划船功能

---
+++ sail/sail.lua
@@ -0,0 +1,15 @@
+-- ========== 設定 ================
+Settings:setCompareDimension(true, 2340)
+Settings:setScriptDimension(true, 2340)
+Settings:set("MinSimilarity", 0.9)
+setImmersiveMode(true)
+autoGameArea(true)
+setManualTouchParameter(20, 1)
+
+regionSail = Region(1300, 630, 300, 280)
+
+while true do
+    regionSail:existsClick("sail.png")
+    regionSail:existsClick("boating.png")
+    wait(2)
+end
