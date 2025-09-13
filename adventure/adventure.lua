d20b1b7677f0a95803bd1c75fb1becc61d906ef9
maxwell.hsu <maxwell5782@hotmail.com>
2025-09-13 22:21:28 +0800

    優化

--- adventure/adventure.lua
+++ adventure/adventure.lua
@@ -7,6 +7,7 @@ autoGameArea(true)
 setManualTouchParameter(20, 1)
 
 interval = 2
+AutoWaitTimeout = 1
 regionFoot = Region(1615, 631, 87, 80)
 
 -- 主要路線
@@ -17,11 +18,11 @@ routes[1] = "採集.png"
 -- 找圖
 function findImage(image, region)
     toast(string.format("findImage(%s, [%s,%s,%s,%s])", image, region.x, region.y, region.w, region.h))
-    result = region:exists(image)
+    result = region:exists(image, AutoWaitTimeout)
     while result == nil do
         wait(findImageInterval)
         toast(string.format("findImage(%s, [%s,%s,%s,%s])", image, region.x, region.y, region.w, region.h))
-        result = region:exists(image)
+        result = region:exists(image, AutoWaitTimeout)
     end
     toast(string.format("found %s", image))
     return region:getLastMatch()
@@ -29,16 +30,14 @@ end
 
 -- 設定
 dialogInit()
+addTextView("找圖間隔(秒)")
+addEditNumber("findImageInterval", 5)
+addTextView("目標路線")
 addRadioGroup("routeIdx", 0)
 for i, name in pairs(routes) do
     addRadioButton(name, i)
 end
-dialogShow("目標路線")
-
-dialogInit()
-addTextView("找圖間隔(秒)")
-addEditNumber("findImageInterval", 5)
-dialogShow("設定")
+dialogShow("")
 
 while true do
     -- 往下移動直到出現腳腳
@@ -49,23 +48,32 @@ while true do
             { action = "touchUp",   target = Location(273, 980) },
             { action = "wait",      target = interval }
         })
-        result = regionFoot:exists("foot.png")
+        result = regionFoot:exists("foot.png", AutoWaitTimeout)
     until result ~= nil
 
     -- 進入副本
     click(regionFoot:getLastMatch())
     click(findImage("enter.png", Region(2024, 965, 83, 46)))
-    Region(1203, 637, 308, 111):existsClick("confirm.png")
+    Region(1203, 637, 308, 111):existsClick("confirm.png", AutoWaitTimeout)
     click(findImage("auto.png", Region(2190, 685, 85, 85)))
     click(findImage("start_auto.png", Region(1569, 841, 141, 35)))
-
-    repeat
-        toast("auto adventure")
-        result = Region(2190, 685, 85, 85):exists("auto.png")
+    wait(2)
+    while true do
+        -- 找不到自動中，可能是在選路線
+        if Region(1173, 1002, 101, 32):exists("自動中.png", AutoWaitTimeout) == nil then 
+            if Region(1083, 110, 182, 43):exists("選擇路線.png", AutoWaitTimeout) == nil then
+                -- 也可能是選完路線的過場
+                result = Region(49, 257, 95, 26):exists("目的地.png", AutoWaitTimeout)
+                if result == nil then
+                    -- 真的是結束自動冒險了
+                    break
+                end
+            end
+        end
         -- 這邊自動冒險中，換層如果有目標路線就點
-        Region(560, 274, 1209, 290):existsClick(routes[routeIdx])
+        Region(560, 274, 1209, 290):existsClick(routes[routeIdx], AutoWaitTimeout)
         wait(findImageInterval)
-    until result ~= nil
+    end
 
     -- 離開副本
     click(findImage("exit.png", Region(1980, 93, 29, 35)))
