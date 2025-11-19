fb3c3a05f011f678cbac340561201194cbefc136
maxwell.hsu <maxwell5782@hotmail.com>
2025-11-19 22:30:35 +0800

    現在會使用測量

--- sail/sail.lua
+++ sail/sail.lua
@@ -7,9 +7,13 @@ autoGameArea(true)
 setManualTouchParameter(20, 1)
 
 regionSail = Region(1300, 630, 300, 280)
+regionSkill = Region(1842, 600, 441, 338)
+regionSwitch = Region(2216, 967, 62, 46)
 
 while true do
-    regionSail:existsClick("sail.png", 1)
-    regionSail:existsClick("boating.png", 1)
+    regionSwitch:existsClick("開關.png", 0.5)
+    regionSkill:existsClick("測量.png", 0.5)
+    regionSail:existsClick("sail.png", 0.5)
+    regionSail:existsClick("boating.png", 0.5)
     wait(4)
 end
